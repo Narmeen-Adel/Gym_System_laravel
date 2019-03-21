b8f3e05139084a8b9a3e1cc533f582ac52e1b260
Youmna <youmnaabdalaziz@gmail.com>
2019-03-21 20:11:22 +0200

    Add forigen keys to coaches_sessions table

--- database/migrations/2019_03_21_175928_create_coaches_sessions_table.php
+++ database/migrations/2019_03_21_175928_create_coaches_sessions_table.php
@@ -16,8 +16,11 @@ class CreateCoachesSessionsTable extends Migration
         Schema::create('coaches_sessions', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('session_id');
+            $table->foreign('session_id')->references('id')->on('sessions');
             $table->unsignedBigInteger('coach_id');
+            $table->foreign('coach_id')->references('id')->on('coaches');
             $table->timestamps();
         });
     }
+
 }
