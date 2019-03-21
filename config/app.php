66d6e489713eda8456dabde698f660f9576a35ed
Youmna <youmnaabdalaziz@gmail.com>
2019-03-21 23:53:36 +0200

    Add countries pkg configration

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        'Webpatser\Countries\CountriesServiceProvider',
 
     ],
 
@@ -227,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'Countries' => 'Webpatser\Countries\CountriesFacade',
 
     ],
 
