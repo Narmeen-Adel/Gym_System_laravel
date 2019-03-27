9515c7902af60a1341e8a958d32168ef1b97d68e
Faten Ibrahim <faten_elssayad@yahoo.com>
2019-03-27 21:04:59 +0200

    add attendance model &controller  for admin & city_manager and gym_manager by using laravel permissions

--- app/Http/Controllers/UserAttendancecontroller.php
+++ app/Http/Controllers/UserAttendancecontroller.php
@@ -7,24 +7,84 @@ use App\Session;
 use App\Customer;
 use App\Attendance;
 use App\CustomerSession;
+use Auth;
 use DB;
 
 class UserAttendancecontroller extends Controller
 {
     public function index()
     {
-        $data=DB::table('customer_session')
-                ->join('sessions','sessions.id','=','customer_session.session_id')
-                ->join('customers','customers.id','=','customer_session.customer_id')
-                ->join('gyms','gyms.id','=','sessions.gym_id')
-                ->join('cities','cities.id','=','gyms.city_id')
-                ->select('customers.name as custm_name','customers.email as email',
-                'sessions.name as session_name','customer_session.attendance_date as attendanceDate',
-                'gyms.name as gym','cities.name as city')
+        $user = \Auth::user();
+        $role = $user->roles->first()->name;
+
+        if ($role === 'admin') {
+            $data = DB::table('customer_session')
+                ->join('sessions', 'sessions.id', '=', 'customer_session.session_id')
+                ->join('customers', 'customers.id', '=', 'customer_session.customer_id')
+                ->join('gyms', 'gyms.id', '=', 'sessions.gym_id')
+                ->join('cities', 'cities.id', '=', 'gyms.city_id')
+                ->select(
+                    'customers.name as custm_name',
+                    'customers.email as email',
+                    'sessions.name as session_name',
+                    'sessions.starts_at as attendanceDate',
+                    // 'customer_session.attendance_date as attendanceDate',
+                    'gyms.name as gym',
+                    'cities.name as city'
+                )
                 ->get();
-        // dd($data);
-        return view('attendance.index', [
-            'histories' => $data
-        ]);
+
+            return view('attendance.index', [
+                'histories' => $data
+            ]);
+        } elseif ($role === 'city_manager') {
+            $id = Auth::user()->id;
+            $city_id = DB::table('cities')
+                ->select('cities.id')
+                ->where('cities.city_manager_id', '=', $id)
+                ->value('id');
+
+            $data = DB::table('customer_session')
+                ->join('sessions', 'sessions.id', '=', 'customer_session.session_id')
+                ->join('customers', 'customers.id', '=', 'customer_session.customer_id')
+                ->join('gyms', 'gyms.id', '=', 'sessions.gym_id')
+                ->select(
+                    'customers.name as custm_name',
+                    'customers.email as email',
+                    'sessions.name as session_name',
+                    'sessions.starts_at as attendanceDate',
+                    // 'customer_session.attendance_date as attendanceDate',
+                    'gyms.name as gym'
+                )->where('gyms.city_id', '=', $city_id)->get();
+
+
+
+            return view('attendance.index', [
+                'histories' => $data
+            ]);
+        } elseif ($role === 'gym_manager') {
+            $id = Auth::user()->id;
+            $gym_id = DB::table('users')
+                ->select('users.gym_id')
+                ->where('users.id', '=', $id)
+                ->value('id');
+                // dd($gym_id);
+            $data = DB::table('customer_session')
+                ->join('sessions', 'sessions.id', '=', 'customer_session.session_id')
+                ->join('customers', 'customers.id', '=', 'customer_session.customer_id')
+                ->join('gyms', 'gyms.id', '=', 'sessions.gym_id')
+                ->select(
+                    'customers.name as custm_name',
+                    'customers.email as email',
+                    'sessions.name as session_name',
+                    'sessions.starts_at as attendanceDate'
+                    // 'customer_session.attendance_date as attendanceDate',
+                )->where('gyms.id', '=', $gym_id)
+                ->get();
+
+            return view('attendance.index', [
+                'histories' => $data
+            ]);
+        }
     }
 }
