0fc65bb6d894296220b8923b7f8fed26df050b85
nadaessa <nessam378@gmail.com>
2019-03-27 19:43:49 +0200

    add two columns to gyms and edit in gyms views

--- resources/views/gyms/create.blade.php
+++ resources/views/gyms/create.blade.php
@@ -2,27 +2,24 @@
 
 @section('content')
 
-
-
 <div class="container con">
     <h2>Add Gym</h2>
 
 <form action="{{route('gyms.store')}}" method="POST">
     @csrf
     <div class="form-group">
-           <label for="exampleInputEmail1">Name</label>
+           <label>Name</label>
            <input name="name" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name">
        </div>
 
-        <div class="form-group">
-           <label for="exampleInputPassword1">City</label>
-           <select class="form-control" name="city_id">
-               @foreach($cities as $city)
-                   <option value="{{$city->id}}">{{$city->name}}</option>
-               @endforeach
-           </select>
-           
-       </div>
+       <div class="form-group">
+            <input name="user_id" type="hidden" value="{{$user->id}}" class="form-control" />
+        </div>
+
+       <div class="form-group">
+            <label>Cover Image</label>
+            <input name="cover_image" type="file" class="form-control" placeholder="Choose Cover Image"/>
+        </div>
 
     <button type="submit" class="btn btn-primary">Submit</button>
     <a href="{{route('gyms.index')}}" class="btn btn-danger">Back</a>
