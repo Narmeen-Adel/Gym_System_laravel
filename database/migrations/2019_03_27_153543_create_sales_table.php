1f678728a3b4bf877f280fd9def095305fda44d5
Faten Ibrahim <faten_elssayad@yahoo.com>
2019-03-27 22:23:17 +0200

    edit add_Id_column_to_sessions migration

--- database/migrations/2019_03_27_153543_create_sales_table.php
+++ database/migrations/2019_03_27_153543_create_sales_table.php
@@ -14,14 +14,12 @@ class CreateSalesTable extends Migration
     public function up()
     {
         Schema::create('sales', function (Blueprint $table) {
-            $table->bigIncrements('id');
             $table->double('paid_price', 8, 2);
             $table->integer('available_sessions');
             $table->unsignedbigInteger('package_id')->nullable();
             $table->foreign('package_id')->references('id')->on('packages')->onDelete('cascade');
             $table->unsignedbigInteger('customer_id')->nullable();
             $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
-            $table->timestamps();
         });
 
     }
