b49bf5770afac5a98d3bf3d7dade09b872f3ec1e
nadaessa <nessam378@gmail.com>
2019-03-29 20:27:50 +0200

    edit in delete

--- resources/views/gyms/index.blade.php
+++ resources/views/gyms/index.blade.php
@@ -51,8 +51,7 @@ var c_array = [
          c_array.push(
             {
                     mRender: function (data, type, row) {
-                        return '<a href="/gyms/'+row.id+'/edit" class=" btn btn-success" data-id="' + row.id + '" style="margin-left:10px;"><i class="fa fa-edit"></i><span>Edit</span></a>'
-                        + '<a href="#" class=" btn btn-danger" row_id="' + row.id + '" data-toggle="modal" data-target="#DeleteModal" id="delete_toggle" style="margin-left:10px;"><i class="fa fa-times"></i><span>Delete</span></a>'
+                        return '<a href="/gyms/'+row.id+'/edit" class=" btn btn-success" data-id="' + row.id + '" style="margin-left:10px;"><i class="fa fa-edit"></i><span>Edit</span></a> <form style="display:inline" method="POST" action="gyms/'+row.id+'">@csrf   {{ method_field('DELETE')}}<button type="submit" onclick="return myFunction();" class="btn btn-xs btn-danger"><i class="fa fa-times"></i>Delete</button></form>'
                     }
                 },
 
@@ -68,28 +67,7 @@ var c_array = [
                 url: '/data_gyms',
                 dataType : 'json',
                 type: 'get',
-<<<<<<< HEAD
-            },
-            columns: [
-                { data: 'id' },
-                { data: 'name' },
-                { data: 'user.name' },
-                { data: 'cover_image' },
-               {
-                    mRender: function (data, type, row) {
-                        return '<a href="/gyms/'+row.id+'/edit" class=" btn btn-success" data-id="' + row.id + '" style="margin-left:10px;"><i class="fa fa-edit"></i><span>Edit</span></a> <form style="display:inline" method="POST" action="gyms/'+row.id+'">@csrf   {{ method_field('DELETE')}}<button type="submit" onclick="return myFunction();" class="btn btn-xs btn-danger"><i class="fa fa-times"></i>Delete</button></form>'
-                    }
-                },
-=======
-                // success:function(response) {
->>>>>>> 8610c066ea5e51f74bdd16fa76ff813f06ef7695
-
-                //     console.log(response);
-                // },
-                // error: function (response) {
-                //     alert(' Cant Save This Documents !');
-                //     console.log(response);
-                // }
+         
             },
             columns: c_array,
             'paging'      : true,
